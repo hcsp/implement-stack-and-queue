c3bcf99e7075999dd84c89b3bd0186f9fb9a851a
Bo Zhang <zhangbodut@gmail.com>
2019-09-16 14:38:13 +0800

    Initial commit

---
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -0,0 +1,11 @@
+package com.github.hcsp.datastructure;
+
+public class Queue {
+    // 将一个元素添加到队列尾部
+    public void add(int value) {}
+
+    // 将一个元素从队列头部移走
+    public int remove() {
+        return 0;
+    }
+}
---
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -0,0 +1,11 @@
+package com.github.hcsp.datastructure;
+
+public class Stack {
+    // 将一个元素压入栈内
+    public void push(int value) {}
+
+    // 从栈顶弹出一个元素
+    public int pop() {
+        return 0;
+    }
+}
