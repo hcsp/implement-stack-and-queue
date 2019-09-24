ea9f7432ade4eccdebf1a058e681805728d3f76e
fcbhank <hankhu4work@163.com>
2019-09-24 22:02:07 +0800

    Stack-DynamicArrays, Queue-LinkedList

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,21 @@
 package com.github.hcsp.datastructure;
 
+import java.util.LinkedList;
+
 public class Queue {
+    // Queue 在 Java 中是一个接口
+    // 常见的实现类为 LinkedList
+    private LinkedList<Integer> list = new LinkedList<>();
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        list.add(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        int first = list.getFirst();
+        list.removeFirst();
+        return first;
     }
 }
