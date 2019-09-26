b3c8a7a9162a3718c987eb0157f17ba4a57c4082
Yuyan <845058547@qq.com>
2019-09-26 13:46:35 +0800

    Use deque to sovle this problem

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,17 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Stack {
+    Deque<Integer> deque = new ArrayDeque<>();
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        deque.addFirst(value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return deque.removeFirst();
     }
 }
