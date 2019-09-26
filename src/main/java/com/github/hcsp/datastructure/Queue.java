008f88210407c1334abdff362d4a04ae0ac48ebf
Scott_Yu <51253421+Scott-YuYan@users.noreply.github.com>
2019-09-26 13:49:55 +0800

    Use deque to sovle this problem (#4)
    Use deque to sovle this problem (#4)

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,18 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Queue {
+    Deque<Integer> deque = new ArrayDeque<>();
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        deque.addLast(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        return deque.removeFirst();
     }
 }
