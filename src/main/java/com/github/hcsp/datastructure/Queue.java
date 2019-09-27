e9cebbc4f2dd2f3d0e1d9b3c405110eb9fa59d81
zeze.li <lizeze.boy@gmail.com>
2019-09-27 16:29:24 +0800

    '堆和栈' (#1)
    '堆和栈' (#1)

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,37 @@
 package com.github.hcsp.datastructure;
 
+
+
 public class Queue {
+    private int[] data;
+    private int size;
+    private int front;
+    private int rear;
+
+
+    public Queue() {
+        data = (new int[10]);
+        size = 0;
+        front =0;
+        rear = 0;
+
+    }
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        rear = (front+size)%data.length;
+        System.out.println(rear);
+        data[rear] = value;
+        size++;
+
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        int tempData = data[front];
+        front = (front + 1) % (data.length);
+        size--;
+        return tempData;
+
     }
 }
--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,21 @@
 package com.github.hcsp.datastructure;
 
 public class Stack {
+    private int maxSize = 100;
+    private int[] stackArray;
+    private int top = -1;
+
+    public Stack() {
+        stackArray=new int[maxSize];
+    }
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        stackArray[++top] = value;
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return stackArray[top--];
     }
 }
