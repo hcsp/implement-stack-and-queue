8a5b8e3f6718ef7180526a715bb812318caf31f1
SuyuZhuang <zhuangsuyu1993@163.com>
2019-09-29 16:10:15 +0800

    使用链表实现栈和队列

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,38 @@
 package com.github.hcsp.datastructure;
 
 public class Queue {
+    Node first = null;
+    Node last = null;
+
+    static class Node {
+        int value;
+        Node next;
+    }
+
+    boolean isEmpty() {
+        return this.first == null;
+    }
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        Node curr = new Node();
+        curr.value = value;
+        if (isEmpty()) {
+            first = curr;
+            last = curr;
+        } else {
+            last.next = curr;
+            last = curr;
+        }
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        int removedValue = first.value;
+        first = first.next;
+        if (isEmpty()) {
+            last = null;
+        }
+        return removedValue;
     }
 }
