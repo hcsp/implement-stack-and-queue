b330c30df43530755deab5c5c487131cf200f5a0
丁胜 <dingduoduo0916@gmail.com>
2019-10-11 22:46:43 +0800

    Dsying (#7)
    Dsying (#7)

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,34 @@
 package com.github.hcsp.datastructure;
 
+import java.util.NoSuchElementException;
+
 public class Queue {
+    static class Node {
+        int val;
+        Node next;
+    }
+
+    Node head = null;
+    Node tail = null;
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        Node curr = new Node();
+        curr.val = value;
+        if (tail == null) {
+            head = curr;
+        }else {
+            tail.next = curr;
+        }
+        tail = curr;
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        int val = head.val;
+        head = head.next;
+        return val;
     }
 }
--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,39 @@
 package com.github.hcsp.datastructure;
 
+import java.util.NoSuchElementException;
+
 public class Stack {
+    static class Node {
+        Node prev;
+        int val;
+        Node next;
+    }
+
+    Node head = null;
+    Node tail = null;
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        Node curr = new Node();
+        curr.val = value;
+
+        if (tail == null) {
+            head = curr;
+        } else {
+            curr.prev = tail;
+            tail.next = curr;
+
+        }
+        tail = curr;
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        if (tail == null) {
+            throw new NoSuchElementException();
+        }
+        int val = tail.val;
+        tail = tail.prev;
+        return val;
     }
 }
