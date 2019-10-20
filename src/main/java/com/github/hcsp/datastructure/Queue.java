8fabf3af391cf8f4ae436223e54e6eecbcadbbd0
doubeans <sean.yang.shen@gmail.com>
2019-10-20 16:40:48 +0800

    debug

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,22 @@
 package com.github.hcsp.datastructure;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Queue {
+    private List<Integer> list;
+
+    public Queue() {
+        list = new LinkedList<>();
+    }
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        list.add(list.size(), value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        return list.remove(0);
     }
 }
