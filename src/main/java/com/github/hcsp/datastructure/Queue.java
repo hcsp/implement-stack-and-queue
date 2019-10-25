e9733b7653f6de5fde8a6de6dc9a2fb0883c25fc
Yuanmeng Xia <xymreasonxxs@163.com>
2019-10-26 01:35:45 +0800

    stack&queue (#11)
    stack&queue (#11)

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,64 @@
 package com.github.hcsp.datastructure;
 
+import java.util.NoSuchElementException;
+
 public class Queue {
+    ListNode dummmyHead;
+    ListNode tail;
+    int size;
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+
+    private class ListNode {
+        ListNode next;
+        int value;
+
+        ListNode(ListNode next, int value) {
+            this.next = next;
+            this.value = value;
+        }
+
+        ListNode() {
+        }
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int getFront() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("队列为空");
+        }
+        return dummmyHead.next.value;
+    }
+
+    public Queue() {
+        this.dummmyHead = new ListNode();
+        this.tail = new ListNode();
+    }
+
+    public void add(int value) {
+        if (dummmyHead.next == null) {
+            ListNode node = new ListNode(null, value);
+            dummmyHead.next = node;
+            tail = node;
+            size++;
+        } else {
+            ListNode node = new ListNode(null, value);
+            tail.next = node;
+            tail = node;
+            size++;
+        }
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        if (isEmpty()) {
+            throw new NoSuchElementException("队列为空");
+        }
+        ListNode rm = dummmyHead.next;
+        dummmyHead.next = dummmyHead.next.next;
+        return rm.value;
     }
 }
+
