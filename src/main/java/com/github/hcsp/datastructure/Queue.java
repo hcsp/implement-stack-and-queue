1d45a6b6c37b2d465eaada7c4e2ce2ad6e225a4f
jinlf <2354833@qq.com>
2019-10-27 00:08:45 +0800

    Queue and Stack

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,30 @@
 package com.github.hcsp.datastructure;
 
 public class Queue {
+    private int[] intQueue;
+    private final int capacity = 100;
+    private int size;
+    private int start;
+    private int end;
+
+    public Queue(){
+        intQueue = new int[capacity];
+        size = 0;
+        start = 0;
+        end = 0;
+    }
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        end = (start + size) % intQueue.length;
+        intQueue[end] = value;
+        size++;
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        int value = intQueue[start];
+        start = (start + 1) % intQueue.length;
+        size--;
+        return value;
     }
 }
--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,31 @@
 package com.github.hcsp.datastructure;
 
+
 public class Stack {
+    private final int size = 1000;
+    private int[] stackArray;
+    private int stackTop = -1;
+
+    public Stack() {
+        stackArray = new int[size];
+    }
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        if (stackTop == size - 1) {
+            System.err.println("The stack is full and the value is discarded!");
+        }else {
+            stackArray[++stackTop] = value;
+        }
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        if (stackTop == -1) {
+            System.err.println("The stack is empty and nothing to pop!");
+            return -999999999;
+        }else {
+            return stackArray[stackTop--];
+        }
+
     }
 }
