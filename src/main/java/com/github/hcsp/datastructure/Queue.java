b384e01c2aefa196b901cf1757e8dfe33119c18f
phlvance <39873702+phlvance@users.noreply.github.com>
2019-11-20 17:57:39 +0800

    implemet stack and queue (#19)
    implemet stack and queue (#19)

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,37 @@
 package com.github.hcsp.datastructure;
 
 public class Queue {
+
+    private int start;
+
+    private int end;
+
+    private int max_size = 16;
+
+    private Object[] data;
+
+    public Queue() {
+        start = end = 0;
+        data = new Object[max_size];
+    }
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        if (end == max_size) {
+            throw new RuntimeException("队列满了");
+        } else {
+            data[end++] = value;
+        }
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        if (start == end) {
+            throw new RuntimeException("队列为空");
+        } else {
+            int num = (int) data[start];
+            data[start++] = null;
+            return num;
+        }
     }
 }
--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,36 @@
 package com.github.hcsp.datastructure;
 
 public class Stack {
+
+    private int top = -1;
+
+    private int max_size = 16;
+
+    private int size;
+
+    private Object[] data;
+
+    public Stack() {
+        data = new Object[max_size];
+    }
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        if (size == max_size) {
+            throw new RuntimeException("栈满了");
+        } else {
+            data[++top] = value;
+            size++;
+        }
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        if (top == -1) {
+            throw new RuntimeException("栈空了");
+        } else {
+            size--;
+            return (int) data[top--];
+        }
     }
 }
