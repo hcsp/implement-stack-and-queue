63e1682426214b0b663d72b61f31fe14ed33670a
acgq <chen330021@live.com>
2019-11-30 20:18:02 +0800

    实现队列和栈

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,18 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Queue {
+    List<Integer> list = new ArrayList<>();
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        list.add(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        return list.remove(0);
     }
 }
--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,18 @@
 package com.github.hcsp.datastructure;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Stack {
+    Deque<Integer> deque = new LinkedList<>();
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        deque.add(value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return deque.removeLast();
     }
 }
