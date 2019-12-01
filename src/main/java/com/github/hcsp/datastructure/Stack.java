845e8932fa59fe9f96b22a74e14e4947463cd4ba
hukaibo <52575259+hukaibo@users.noreply.github.com>
2019-12-01 12:36:17 +0800

    栈和队列 (#22)
    栈和队列 (#22)

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,18 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stack {
+    List<Integer> queue=new ArrayList<>();
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+         queue.add(value);
+
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return queue.remove(queue.size()-1);
     }
 }
