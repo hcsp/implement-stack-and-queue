fc5958254eb6eae235c548fed26b63aae2af1bca
adanthony53 <32273951+adanthony53@users.noreply.github.com>
2019-12-01 16:23:35 -0800

    implements stack with arraylist and queue with linkedlist

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,21 +1,19 @@
 package com.github.hcsp.datastructure;
 
+import java.util.*;
+
 public class Queue {
     // 将一个元素添加到队列尾部
-    private int[] arr = new int[Integer.MAX_VALUE];
-    int size = arr.length;
-    int start = 0;
-    int end = 0;
+    LinkedList<Integer> queue = new LinkedList<>();
 
     public void add(int value) {
-        end = (end+1) % size;
-        arr[end] = value;
+        queue.add(value);
     }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        int res = arr[start];
-        start = (start+1) % size;
+        int res = queue.get(0);
+        queue.remove();
         return res;
     }
 }
--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,16 +1,19 @@
 package com.github.hcsp.datastructure;
 
+import java.util.*;
+
 public class Stack {
     // 将一个元素压入栈内
-    private int[] arr = new int[Integer.MAX_VALUE];
-    private int top = 0;
+    List<Integer> stack = new ArrayList<>();
 
     public void push(int value) {
-        arr[top++] = value;
+        stack.add(value);
     }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return arr[top--];
+        int res = stack.get(stack.size()-1);
+        stack.remove(stack.size()-1);
+        return res;
     }
 }
