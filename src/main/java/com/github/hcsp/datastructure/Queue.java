dd9000ffb5365d7b04c988e19dee8414ca04c6a1
adanthony53 <32273951+adanthony53@users.noreply.github.com>
2019-12-01 16:24:09 -0800

    implements stack and queue with array (#23)
    implements stack and queue with array (#23)

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,19 @@
 package com.github.hcsp.datastructure;
 
+import java.util.*;
+
 public class Queue {
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    LinkedList<Integer> queue = new LinkedList<>();
+
+    public void add(int value) {
+        queue.add(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        int res = queue.get(0);
+        queue.remove();
+        return res;
     }
 }
--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,19 @@
 package com.github.hcsp.datastructure;
 
+import java.util.*;
+
 public class Stack {
     // 将一个元素压入栈内
-    public void push(int value) {}
+    List<Integer> stack = new ArrayList<>();
+
+    public void push(int value) {
+        stack.add(value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        int res = stack.get(stack.size()-1);
+        stack.remove(stack.size()-1);
+        return res;
     }
 }
