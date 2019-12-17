b295ea4d8826206a418f298e80b9cfd27db1b9be
shenwl <shenweilun96@gmail.com>
2019-12-17 21:36:54 +0800

    栈和队列 (#25)
    栈和队列 (#25)

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,41 @@
 package com.github.hcsp.datastructure;
 
 public class Queue {
+    private int capacity = 50;
+    private int count = 0;
+    private int[] arr = new int[capacity];
+
+    public void resize(int newSize) {
+        int[] temp = new int[newSize];
+
+        System.arraycopy(arr, 0, temp, 0, count);
+
+        capacity = newSize;
+        arr = temp;
+    }
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        if (capacity - count < 1) {
+            resize(capacity * 2);
+        }
+        arr[count] = value;
+        count++;
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        if (count < 1) {
+            throw new RuntimeException("queue is empty!");
+        }
+        if (count < capacity / 4) {
+            resize(capacity / 2);
+        }
+        int[] temp = new int[capacity];
+        int removeEl = arr[0];
+        System.arraycopy(arr, 1, temp, 0, count - 1);
+        arr = temp;
+        count--;
+        return removeEl;
     }
 }
--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,41 @@
 package com.github.hcsp.datastructure;
 
 public class Stack {
+    private int capacity = 50;
+    private int count = 0;
+    private int[] arr = new int[capacity];
+
+    public void resize(int newSize) {
+        int[] temp = new int[newSize];
+
+        System.arraycopy(arr, 0, temp, 0, count);
+
+        capacity = newSize;
+        arr = temp;
+    }
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        if (capacity - count < 1) {
+            resize(capacity * 2);
+        }
+        arr[count] = value;
+        count++;
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        if (count < 1) {
+            throw new RuntimeException("stack is empty!");
+        }
+        if (count < capacity / 4) {
+            resize(capacity / 2);
+        }
+        int[] temp = new int[capacity];
+        int removeEl = arr[count - 1];
+        System.arraycopy(arr, 0, temp, 0, count - 1);
+        arr = temp;
+        count--;
+        return removeEl;
     }
 }
