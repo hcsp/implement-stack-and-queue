ada29aa27e85271c8d9ac8d55eae542b3dfb3410
tongkang <actongkang@outlook.com>
2019-12-29 21:57:35 +0800

    实现队列和栈 (#27)
    实现队列和栈 (#27)

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,49 @@
 package com.github.hcsp.datastructure;
 
 public class Queue {
+    LiQueue front = null;
+    LiQueue rear = null;
+
+    public boolean isQueueEmpty() {
+        if (this.front == null || this.rear == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        LiQueue temp = new LiQueue();
+        temp.value = value;
+        temp.next = null;
+        if (isQueueEmpty()) {
+            rear = front = temp;
+        } else {
+            rear.next = temp;
+            rear = temp;
+        }
+
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        LiQueue temp = new LiQueue();
+        if (isQueueEmpty()) {
+            return 0;
+        } else {
+            temp = front;
+        }
+        if (rear == front) {
+            rear = front = null;
+        } else {
+            front = front.next;
+        }
+        return temp.value;
     }
+}
+
+class LiQueue {
+    int value;
+    LiQueue next;
 }
--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,29 @@
 package com.github.hcsp.datastructure;
 
 public class Stack {
+    private Node top;
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        Node temp = new Node();
+        temp.value = value;
+        temp.next = top;
+        top = temp;
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        if (top.next == null) {
+            return 0;
+        }
+        int value = top.value;
+        top = top.next;
+        return value;
     }
+
+
+}
+class Node {
+    int value;
+    Node next;
 }
