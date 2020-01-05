aa4ece97cadd0b60c4917e663902f372a77655cb
baixc <969552017@qq.com>
2020-01-05 21:04:00 +0800

    实现栈和队列

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,18 @@
 package com.github.hcsp.datastructure;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Stack {
+    private List list = new LinkedList();
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        list.add(value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return (int) list.remove(list.size() - 1);
     }
 }
