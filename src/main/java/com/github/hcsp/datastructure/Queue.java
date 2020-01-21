fc30d0a70d1b6f2e51808f4bad9137ab16c3239f
PingAn_NetBar <840495823@qq.com>
2020-01-22 00:58:11 +0800

    简单模拟Stack和Queue (#30)
    简单模拟Stack和Queue (#30)

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,24 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+
 public class Queue {
+    private ArrayList<Integer> capacity = new ArrayList<Integer>();
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        capacity.add(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        if (isEmpty()){
+            return -1;
+        }
+        return capacity.remove(0);
+    }
+
+    public boolean isEmpty() {
+        return capacity.size() == 0;
     }
 }
--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,24 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+
 public class Stack {
+    private ArrayList<Integer> capacity = new ArrayList<Integer>();
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        capacity.add(value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        if (isEmpty()){
+            return -1;
+        }
+        return capacity.remove(capacity.size()-1);
+    }
+
+    public boolean isEmpty() {
+        return capacity.size() == 0;
     }
 }
