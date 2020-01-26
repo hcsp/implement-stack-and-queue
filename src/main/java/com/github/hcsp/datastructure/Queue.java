7102c9ed34b399c3659a6f0c5acfb01ad0b21188
Xi Pu <xp19@duke.edu>
2020-01-26 12:23:38 -0500

    implement queue and stack

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,31 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Queue {
+
+    Deque<Integer> stackOne;
+    Deque<Integer> stackTwo;
+
+    public Queue() {
+        stackOne = new ArrayDeque<>();
+        stackTwo = new ArrayDeque<>();
+    }
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        stackOne.push(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        if (stackTwo.isEmpty()) {
+            while (!stackOne.isEmpty()) {
+                stackTwo.push(stackOne.pop());
+            }
+        }
+
+        return stackTwo.pop();
     }
 }
