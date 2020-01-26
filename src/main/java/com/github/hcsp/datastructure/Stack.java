c0c4f32d470e423808dc7e6d49c3ebb0cea10a42
Chenghao Zheng <45369893+NervousOrange@users.noreply.github.com>
2020-01-26 16:05:16 +0100

    from NervousOrange: Assignment 3 of data structure. (#31)
    from NervousOrange: Assignment 3 of data structure. (#31)

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,27 @@
 package com.github.hcsp.datastructure;
 
+import java.util.Arrays;
+
 public class Stack {
+    private int[] elements;
+
+    public Stack() {
+        this.elements = new int[0];
+    }
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        int[] newArray = new int[elements.length + 1];
+        System.arraycopy(elements, 0, newArray, 0, elements.length);
+        newArray[elements.length] = value;
+        elements = newArray;
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        int tail = elements[elements.length - 1];
+        elements = Arrays.copyOf(elements,
+                elements.length - 1);
+        return tail;
     }
 }
