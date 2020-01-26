ed34e05299c156dfb35a5a91c2f82e3466d8fe9b
Chenghao Zheng <45369893+NervousOrange@users.noreply.github.com>
2020-01-26 17:09:07 +0100

    from NervousOrange: Assignment 3 of data structure. refine the Code (#32)
    from NervousOrange: Assignment 3 of data structure. refine the Code (#32)

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,44 @@
 package com.github.hcsp.datastructure;
 
 public class Queue {
+    private int[] elements;
+    private int first;
+    private int last;
+
+    public Queue() {
+        this.elements = new int[8];
+        this.first = 1;
+        this.last = 1;
+    }
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        elements[last] = value;
+        last = (last + elements.length - 1) % elements.length;
+        if (last == first) {
+            resize(elements);
+        }
+    }
+
+    private void resize(int[] elements) {
+        int[] newArray = new int[elements.length * 2];
+        int newLast = last;
+        for (int i = 0; i < elements.length; i++) {
+            newArray[newLast] = elements[last];
+            last = (last + elements.length - 1) % elements.length;
+            newLast = (newLast + newArray.length - 1) % newArray.length;
+        }
+        this.last = newLast;
+        this.elements = newArray;
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        if (elements.length == 0) {
+            System.out.println("There is not element in the Queue.");
+        }
+        int head = elements[first];
+        first = (first + elements.length - 1) % elements.length;
+        return head;
     }
 }
--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,21 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+
 public class Stack {
+    private ArrayList<Integer> elements;
+
+    public Stack() {
+        this.elements = new ArrayList<>();
+    }
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        elements.add(value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return elements.remove(elements.size() - 1);
     }
 }
