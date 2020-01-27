07a818967fb32d8646855b45d1d47f8e8b37643b
Xi Pu <30915281+XiPu99@users.noreply.github.com>
2020-01-26 21:24:36 -0500

    implement queue and stack (#33)
    implement queue and stack (#33)

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,37 @@
 package com.github.hcsp.datastructure;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Stack {
+
+    Queue<Integer> queue;
+
+    public Stack() {
+        queue = new LinkedList<>();
+    }
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        queue.offer(value);
+        int size = queue.size();
+
+        while (size-- > 1) {
+            queue.offer(queue.poll());
+        }
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return queue.poll();
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+
     }
 }
