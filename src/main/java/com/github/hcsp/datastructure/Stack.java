44da5641682d43d85512dca5c2f97bc89db9eed3
zaker-666 <1075151259china@gmail.com>
2020-01-27 12:52:28 +0800

    finish and push

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,19 @@
 package com.github.hcsp.datastructure;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Stack {
+    private final List<Integer> value = new LinkedList<>();
+    int top = 0;
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        this.value.add(top++, value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return this.value.get(--top);
     }
 }
