8024d80375e670f92d62e8b00d22b9622114a61a
zaker-666 <1075151259china@gmail.com>
2020-01-27 12:54:20 +0800

    finish and push (#34)
    finish and push (#34)

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,20 @@
 package com.github.hcsp.datastructure;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Queue {
+    private final List<Integer> value = new LinkedList<>();
+    int start = 0;
+    int end = 0;
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        this.value.add(end++, value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        return this.value.get(start++);
     }
 }
