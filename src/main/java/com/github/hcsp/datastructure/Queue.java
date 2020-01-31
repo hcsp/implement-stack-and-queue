b647ac1a80d68c5937b6cc44e4d5de9ff1e4cce2
zhy <809775352qq.com>
2020-01-31 12:15:25 +0800

    数据结构练习：实现栈和队列

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,17 @@
 package com.github.hcsp.datastructure;
 
+import java.util.LinkedList;
+
 public class Queue {
+    LinkedList<Integer> queue = new LinkedList<>();
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        this.queue.add(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        return this.queue.remove();
     }
 }
