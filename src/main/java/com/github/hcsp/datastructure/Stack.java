562eef9d1c277c2898d2b923cb70f2a715bff6ed
xdc <1434412784@qq.com>
2020-02-02 10:43:10 +0800

    用List 完成栈的数据结构

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,19 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stack {
+
+    List<Integer> stack = new ArrayList<>();
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        this.stack.add(value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return this.stack.remove(stack.size() - 1);
     }
 }
