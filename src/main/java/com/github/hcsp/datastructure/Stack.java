ce1bbabe6962ecf0ec062bc6452bde66d9ae6a63
wangyujie1207 <82414199@163.com>
2020-02-15 15:30:11 +0800

    参考楼下兄台的确实简单 (#41)
    参考楼下兄台的确实简单 (#41)

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,17 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stack {
+    private List<Integer> list = new ArrayList<>();
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        list.add(value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return list.remove(list.size()-1);
     }
 }
