193fe481f6c839014bc12fb1a555685325e24655
Yuzuki-N <nanami2962@gmail,com>
2020-02-17 11:05:21 +0800

    模拟队列和栈

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,22 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Queue {
+
+    List<Integer> queue = new ArrayList<Integer>();
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        queue.add(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        if (queue.isEmpty()) {
+            return -1;
+        }
+        return queue.remove(0);
     }
 }
