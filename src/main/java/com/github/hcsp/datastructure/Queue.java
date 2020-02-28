4235b65b6e91947b1ef0da60f09c2d2203ff44cb
JN0916 <56193588+JN0916@users.noreply.github.com>
2020-02-28 12:09:17 +0800

    实现栈和队列 (#46)
    实现栈和队列 (#46)

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,23 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+
 public class Queue {
+    private ArrayList<Integer> arrayList;
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        if (arrayList == null) {
+            arrayList = new ArrayList<>();
+        }
+        arrayList.add(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        if (arrayList != null && arrayList.size() > 0) {
+            return arrayList.remove(0);
+        }
+        return Integer.parseInt(null);
     }
 }
