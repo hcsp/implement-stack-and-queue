e7e784707407b5d24762e1d1effcba915ae931f9
xuanzebin <xuanzebin@jd.com>
2020-03-01 17:24:02 +0800

    feat: implement stack and queue

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,22 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Queue {
+    private List<Integer> queueList = new ArrayList<>();
+
+    public List<Integer> getQueueList() {
+        return queueList;
+    }
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        this.getQueueList().add(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        return this.getQueueList().remove(0);
     }
 }
