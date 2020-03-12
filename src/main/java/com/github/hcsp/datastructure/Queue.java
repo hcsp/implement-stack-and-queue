1efeeb3564cfc1407014d5c364dc760558fef48e
Lee <lizhijie_lee@126.com>
2020-03-12 16:25:06 +0800

    仿造一个stack和queue数据结构

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,17 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+
 public class Queue {
+    private ArrayList<Integer> queue = new ArrayList<>();
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        queue.add(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        return queue.remove(0);
     }
 }
