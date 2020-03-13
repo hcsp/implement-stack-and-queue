4c628220b2eb0881b3d6a738820972eae9971651
KisetsuLee <42855253+KisetsuLee@users.noreply.github.com>
2020-03-13 10:35:10 +0800

    仿造一个stack和queue数据结构 (#51)
    仿造一个stack和queue数据结构 (#51)

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,19 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+
 public class Stack {
+
+    private ArrayList<Integer> stack = new ArrayList<>();
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+
+        stack.add(value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return stack.remove(stack.size() - 1);
     }
 }
