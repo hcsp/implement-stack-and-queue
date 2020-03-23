e7633fb03555cfb7d670da35c508cdfcb460ab51
huiquan99 <53985556+huiquan99@users.noreply.github.com>
2020-03-23 22:51:10 +0800

    实现栈和队列 (#54)
    实现栈和队列 (#54)

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,17 @@
 package com.github.hcsp.datastructure;
 
-public class Stack {
-    // 将一个元素压入栈内
-    public void push(int value) {}
+import java.util.ArrayList;
+import java.util.List;
 
+public class Stack {
+    List<Integer> arrayList = new ArrayList<>();
+
+    // 将一个元素压入栈内
+    public void push(int value) {
+        arrayList.add(value);
+    }
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return arrayList.remove(arrayList.size() - 1);
     }
 }
