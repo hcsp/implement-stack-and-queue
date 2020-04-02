45b960500cbbf9347f90600dae13e41a98d4c9f4
pathjh <1639925487@qq.com>
2020-04-02 11:52:56 +0800

    实现栈和对列 (#56)
    实现栈和对列 (#56)

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,27 @@
 package com.github.hcsp.datastructure;
 
+import java.util.Stack;
+
+//不使用数组模拟队列，这里使用两个栈来模拟一个队列的插入和删除
 public class Queue {
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        stack1.push(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+
+        if (!stack2.isEmpty()) {
+            return stack2.pop();
+        } else {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+            return stack2.pop();
+        }
     }
 }
