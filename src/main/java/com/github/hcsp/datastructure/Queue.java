3ee3b6ab653b4813e05bf64611c9fe427ffe4785
samuel <zhangleven@gmail.com>
2020-04-09 20:50:26 +0800

    practise stack and queue

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,22 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Queue {
+    private List<Integer> elements;
+
+    public Queue() {
+        elements = new ArrayList<>();
+    }
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        elements.add(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        return elements.remove(0);
     }
 }
