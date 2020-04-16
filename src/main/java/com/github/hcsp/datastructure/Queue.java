3d00034ed140092bb62c7a0f08a03475889af1d3
zhangsht <z429882801@qq.com>
2020-04-16 13:11:06 +0800

    数据结构练习：实现栈和队列

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,35 @@
 package com.github.hcsp.datastructure;
 
+import java.util.Arrays;
+
 public class Queue {
+
+    private int tail = -1;//-1表示为空栈
+
+    private int capacity = 8;//默认容量为8
+
+    private int[] array;//存放数据
+
+    public Queue() {
+        array = new int[capacity];
+    }
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        //array[head] = value;
+        if (++tail >= capacity) {
+            capacity <<= 1;
+            array = Arrays.copyOf(array, capacity);
+        }
+        array[tail] = value;
+
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        int head = array[0];
+        array = Arrays.copyOfRange(array, 1, tail + 1);
+        tail--;
+        return head;
     }
 }
--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,34 @@
 package com.github.hcsp.datastructure;
 
+import java.util.Arrays;
+
 public class Stack {
+
+    private int top = -1;//-1表示为空栈
+
+    private int capacity = 8;//默认容量为8
+
+    private int[] array;//存放数据
+
+    public Stack() {
+        this.array = new int[capacity];
+    }
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        //如果超出容量，扩容
+        if (++top >= capacity) {
+            capacity <<= 1;
+            array = Arrays.copyOf(array, capacity);
+        }
+        array[top] = value;
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        int pop = array[top];
+        array = Arrays.copyOf(array, top--);
+        capacity = array.length;
+        return pop;
     }
 }
