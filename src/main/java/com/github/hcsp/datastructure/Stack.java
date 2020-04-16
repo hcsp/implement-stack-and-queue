2aad1015f2c94cc1e578dd157800ccf2bed7e9df
leo2c <53816081+leo2c@users.noreply.github.com>
2020-04-16 13:23:00 +0800

    数据结构练习：实现栈和队列 (#62)
    数据结构练习：实现栈和队列 (#62)

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,37 @@
 package com.github.hcsp.datastructure;
 
+import java.util.Arrays;
+
 public class Stack {
+
+    private int top = -1;
+
+    private int capacity = 8;
+
+    private int[] array;
+
+    public Stack() {
+        this.array = new int[capacity];
+    }
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        ensureCapacity();
+        array[top] = value;
+    }
+
+    private void ensureCapacity() {
+        if (++top >= capacity) {
+            capacity <<= 1;
+            array = Arrays.copyOf(array, capacity);
+        }
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        int pop = array[top];
+        array = Arrays.copyOf(array, top--);
+        capacity = array.length;
+        return pop;
     }
 }
