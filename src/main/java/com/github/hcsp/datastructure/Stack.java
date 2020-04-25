d5c82ff0905adf69a550d48971d261b9769b0600
arsonalXxu <897386402@qq.com>
2020-04-25 21:08:15 +0800

    修改数组扩容大小 (#64)
    修改数组扩容大小 (#64)

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,36 @@
 package com.github.hcsp.datastructure;
 
+import java.util.Arrays;
+
 public class Stack {
+    int[] elements;
+    int count;
+    int size;
+
+    public Stack() {
+        size = 8;
+        elements = new int[size];
+        count = 0;
+    }
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        if (count == size) {
+            size = size << 1;
+            int[] tmp = elements;
+            elements = Arrays.copyOf(tmp, size);
+        }
+        elements[count] = value;
+        count++;
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        if (count == 0) {
+            throw new RuntimeException("当前栈为空，无元素弹出");
+        }
+        int result = elements[count - 1];
+        count--;
+        return result;
     }
 }
