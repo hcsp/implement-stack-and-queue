6b6a86a78e4d6bd8118236db4b8b436d20b970a2
王文哲 <weidou.wwz@raycloud.com>
2020-05-06 16:57:26 +0800

    实现栈和队列
    直接调用的api。。是不是做法太取巧了。

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,18 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Queue {
+    private Deque<Integer> queue = new ArrayDeque<Integer>();
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        queue.add(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        return queue.remove();
     }
 }
