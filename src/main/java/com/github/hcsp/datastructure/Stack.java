672fc38a7457c5ae43f0ff4443363eb93d6304f8
mysteryven <33973865+mysteryven@users.noreply.github.com>
2020-05-06 17:01:39 +0800

    实现栈和队列 (#66)
    实现栈和队列 (#66)

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,18 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Stack {
     // 将一个元素压入栈内
-    public void push(int value) {}
+    Deque<Integer> stack = new ArrayDeque<Integer>();
+
+    public void push(int value) {
+        stack.push(value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return stack.pop();
     }
 }
