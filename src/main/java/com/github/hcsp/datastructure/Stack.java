7551f2dd39a6b5790a379716d35a60fadecbe372
gofocus <go_focus@live.cn>
2020-05-07 16:39:09 +0800

    简单实现栈和队列

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,26 @@
 package com.github.hcsp.datastructure;
 
+import java.util.Arrays;
+
 public class Stack {
+    private int[] elementData = new int[1000];
+    private int size = 0;
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        elementData[size] = value;
+        size++;
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        if (size > 0) {
+            int value = elementData[size - 1];
+            elementData = Arrays.copyOfRange(elementData, 0, size - 1);
+            size--;
+            return value;
+        }
+        return -1;
     }
+
 }
