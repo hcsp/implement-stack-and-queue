c80b2738a3ef08b2ad6c3309ab5dfee1197027a6
JonesWilde <davidgluttongk@gmail.com>
2020-05-18 12:44:50 -0700

    实现栈和队列

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,17 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+
 public class Stack {
+    ArrayList<Integer> arr = new ArrayList<>();
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        arr.add(value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return arr.remove(arr.size() - 1);
     }
 }
