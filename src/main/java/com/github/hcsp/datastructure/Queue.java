8a997a6e78f733b5f0f340ec004e98b77023c83b
Airtoit <51944383+Airtoit@users.noreply.github.com>
2020-05-19 12:23:24 +0800

    链表实现队列和栈 (#70)
    链表实现队列和栈 (#70)

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,37 @@
 package com.github.hcsp.datastructure;
 
 public class Queue {
+    private Node head;
+    private Node tail;
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        if (head == null) {
+            head = new Node(value);
+            tail = head;
+        } else {
+            tail.next = new Node(value);
+            tail = tail.next;
+        }
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        if (tail == null) {
+            throw new RuntimeException("队列为空！");
+        }
+        Node node = head;
+        head = head.next;
+        return node.value;
+    }
+
+}
+
+class Node {
+    Node next;
+    int value;
+
+    Node(int value) {
+        this.value = value;
     }
 }
--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,26 @@
 package com.github.hcsp.datastructure;
 
 public class Stack {
+    private Node current;
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        if (current == null) {
+            current = new Node(value);
+        } else {
+            Node node = new Node(value);
+            node.next = current;
+            current = node;
+        }
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        if (current == null) {
+            throw new RuntimeException("栈为空");
+        }
+        Node node = current;
+        current = current.next;
+        return node.value;
     }
 }
