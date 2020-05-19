2d5734e4d6fac624336f07f9e2b24c367451dbe3
luyi <luyi95@foxmail.com>
2020-05-19 12:17:40 +0800

    链表实现队列和栈

---
+++ src/main/java/com/github/hcsp/datastructure/Node.java
@@ -0,0 +1,16 @@
+package com.github.hcsp.datastructure;
+
+/**
+ * @author ly
+ */
+public class Node {
+    Node next;
+    int value;
+
+    public Node() {
+    }
+
+    public Node(int value) {
+        this.value = value;
+    }
+}
