95930947079c708832683dbc9977775c1ca5b582
PudgeWL <nbhaohao@gmail.com>
2020-05-31 14:32:25 +0800

    使用 arrayList API 实现 (#71)
    使用 arrayList API 实现 (#71)

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,23 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stack {
     // 将一个元素压入栈内
-    public void push(int value) {}
+
+    List<Integer> list;
+
+    public Stack() {
+        list = new ArrayList<>();
+    }
+
+    public void push(int value) {
+        list.add(value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return list.remove(list.size() - 1);
     }
 }
