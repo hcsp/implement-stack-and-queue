a9eeeacc9e83d6201299851c1ec39b6ce73a87d5
yaohengfeng <1065435454@qq.com>
2020-06-14 18:16:42 +0800

    å®完成了栈和队列的练习

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,18 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+
 public class Queue {
+    ArrayList<Integer> arrays = new ArrayList<>();
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        arrays.add(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+
+        return arrays.remove(0);
     }
 }
