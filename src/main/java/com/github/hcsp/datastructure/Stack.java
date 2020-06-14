ee2f38b9e4567160e956f115d7698fbd64145cee
yaohengfeng <1065435454@qq.com>
2020-06-14 18:18:08 +0800

    完成了栈和队列的练习 (#74)
    完成了栈和队列的练习 (#74)

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,18 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+
 public class Stack {
+
+    ArrayList<Integer> arrays = new ArrayList<>();
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        arrays.add(value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return arrays.remove(arrays.size() - 1);
     }
 }
