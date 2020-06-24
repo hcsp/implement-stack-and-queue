7bdf03a03627d345256c5a258c98c7d0f13baaa1
Haixiang <haixiang6123@gmail.com>
2020-06-24 21:33:20 +0800

    实现栈和队列

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,22 @@
 package com.github.hcsp.datastructure;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Queue {
+    List<Integer> linkedList = new LinkedList<>();
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        linkedList.add(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        if (linkedList.isEmpty()) {
+            return -1;
+        }
+
+        return linkedList.remove(0);
     }
 }
