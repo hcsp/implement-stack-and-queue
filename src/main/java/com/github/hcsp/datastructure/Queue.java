d2b3a036c3dd732879289938a0452b6ba3054c4c
luhao <linesahao@outlook.com>
2020-06-29 23:50:36 +0800

    通过链表和Array数组实现了Stack和Queue。

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,16 @@
 package com.github.hcsp.datastructure;
 
+import java.util.LinkedList;
+
 public class Queue {
+    private LinkedList<Integer> arrayList = new LinkedList<>();
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        arrayList.add(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        return arrayList.removeFirst();
     }
 }
