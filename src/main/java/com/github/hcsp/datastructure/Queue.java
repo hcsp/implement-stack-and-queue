d7cbc9bace2500dd8c0f7ae62085d3b320d5c583
lance <491799643@qq.com>
2020-07-04 21:33:12 +0800

    实现栈跟队列

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,14 @@
 package com.github.hcsp.datastructure;
+import java.util.ArrayList;
 
 public class Queue {
+    ArrayList<Integer> arr = new ArrayList<>();
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
-
+    public void add(int value) {
+        arr.add(value);
+    }
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        return arr.remove(0);
     }
 }
