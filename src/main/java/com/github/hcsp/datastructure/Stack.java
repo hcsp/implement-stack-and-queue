8bbc61a8f92d64c34a1dc9153d76b145e6ba435a
YingHuaLiu <1042413535@qq.com>
2020-07-04 21:10:19 +0800

    队列和栈

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,21 @@
 package com.github.hcsp.datastructure;
 
+import java.util.LinkedList;
+
 public class Stack {
+    LinkedList<Integer> linkedList;
+
+    public Stack() {
+        linkedList = new LinkedList<>();
+    }
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        linkedList.push(value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return linkedList.pop();
     }
 }
