1c3aaa04ea94be7e3e5b1ec80736c93f308b9f2f
concise <15521608275@163.com>
2020-07-21 18:34:21 +0800

    队列和栈

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,20 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Queue {
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
 
-    // 将一个元素从队列头部移走
+    List<Object> list = new ArrayList<>();
+
+    public void add(int value) {
+        list.add(value);
+    }
+
+    // 将一个元素从队列头部移走,第一个元素的索引值是0
     public int remove() {
-        return 0;
+        return (int) list.remove(0);
     }
 }
