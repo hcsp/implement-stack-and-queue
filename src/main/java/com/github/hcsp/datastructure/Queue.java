32db498a6118558f6ef1de41013a345949d2af46
Ryan <2433005622@qq.com>
2020-08-18 18:26:43 +0800

    使用链表方式实现队列和栈

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,39 @@
 package com.github.hcsp.datastructure;
 
 public class Queue {
+    private Node head = null;
+    private Node tail = null;
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        Node node = new Node(value);
+        if (head == null && tail == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+    }
+
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        if (head == null) {
+            return 0;
+        }
+        int value = head.value;
+        head = head.next;
+        return value;
+    }
+
+}
+
+class Node {
+    Node next;
+    int value;
+
+    Node(int value) {
+        this.value = value;
     }
 }
--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,24 @@
 package com.github.hcsp.datastructure;
 
 public class Stack {
+    private Node top = null;
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        Node node = new Node(value);
+        node.next = top;
+        top = node;
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        if (top == null) {
+            return 0;
+        }
+        int value = top.value;
+        top = top.next;
+        return value;
     }
+
 }
+
