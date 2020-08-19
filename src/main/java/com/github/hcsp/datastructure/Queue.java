14925ee17c6a8b0049e6e383ab15b734a55a2c52
cyunt <chenyuntao1997@163.com>
2020-08-19 17:00:09 +0800

    实现栈和队列

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,39 @@
 package com.github.hcsp.datastructure;
 
+
 public class Queue {
+    private Node head;
+    private Node tail;
+
+    class Node {
+        private Node next;
+        private int value;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        Node node = new Node(value);
+        if (head == null && tail == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        int temp = 0;
+        if (head != null) {
+            temp = head.value;
+            head = head.next;
+        }
+        return temp;
     }
 }
--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,32 @@
 package com.github.hcsp.datastructure;
 
+
 public class Stack {
+    private Node top;
+
+    class Node {
+        private Node next;
+        private int value;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        Node node = new Node(value);
+        node.next = top;
+        top = node;
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        int temp = 0;
+        if (top != null) {
+            temp = top.value;
+            top = top.next;
+        }
+        return temp;
     }
 }
