57d5d52d816b67baa9360237051825c688fce5a0
DobbyKim <1175088275@qq.com>
2020-08-23 00:12:22 +0800

    使用数组实现队列 & 栈

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,40 @@
 package com.github.hcsp.datastructure;
 
 public class Queue {
+
+    private Integer[] data;
+    private Integer front;
+    private Integer tail;
+    private Integer size;
+
+    private Queue(int initSize) {
+        if (initSize <= 0) {
+            throw new IllegalArgumentException("队列的初始长度应至少为1");
+        }
+        this.front = 0;
+        this.tail = 0;
+        this.size = 0;
+        this.data = new Integer[initSize];
+    }
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        if (size == data.length) {
+            throw new ArrayIndexOutOfBoundsException("队列已满");
+        }
+        data[tail] = value;
+        tail = tail == data.length - 1 ? 0 : tail + 1;
+        size++;
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        if (size == 0) {
+            throw new ArrayIndexOutOfBoundsException("队列为空");
+        }
+        Integer res = data[front];
+        front = front == data.length - 1 ? 0 : front + 1;
+        size--;
+        return res;
     }
 }
--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,32 @@
 package com.github.hcsp.datastructure;
 
+
 public class Stack {
+
+    private Integer[] data;
+    private Integer size;
+
+    public Stack(int initSize) {
+        if (initSize <= 0) {
+            throw new IllegalArgumentException("栈的初始长度至少应为1");
+        }
+        this.size = 0;
+        this.data = new Integer[initSize];
+    }
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        if (size == data.length) {
+            throw new ArrayIndexOutOfBoundsException("栈已满");
+        }
+        this.data[size++] = value;
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        if (size == 0) {
+            throw new ArrayIndexOutOfBoundsException("栈已空");
+        }
+        return this.data[--size];
     }
 }
