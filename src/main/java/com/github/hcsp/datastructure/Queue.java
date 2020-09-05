830241159c25b9f64c13280cf30c237ec261dd75
wangbo <1216476406@qq.com>
2020-09-05 12:30:57 +0800

    使用 ArrayList 实现队列和栈

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,21 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+
 public class Queue {
+
+    public static ArrayList<Integer> queue = new ArrayList<>();
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        queue.add(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        if (queue.isEmpty()) {
+            return -1;
+        }
+        return queue.remove(0);
     }
 }
--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,20 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+
 public class Stack {
+    public static ArrayList<Integer> queue = new ArrayList<>();
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        queue.add(value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        if (queue.isEmpty()) {
+            return -1;
+        }
+        return queue.remove(queue.size() - 1);
     }
 }
