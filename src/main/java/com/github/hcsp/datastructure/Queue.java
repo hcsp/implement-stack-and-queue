caddcb0abda1ca994c934a2a71532e070710743f
Su <sgz886@gmail.com>
2020-12-17 22:18:15 +0800

    用1个node实现 queue和 stack

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -3,15 +3,49 @@ package com.github.hcsp.datastructure;
 import java.util.LinkedList;
 
 public class Queue {
-    LinkedList<Integer> simpleQueue = new LinkedList<>();
+    private Node head = null;
 
     // 将一个元素添加到队列尾部
     public void add(int value) {
-        simpleQueue.addLast(value);
+        Node toAdd = new Node(value);
+        if (head == null) {
+            head = toAdd;
+            return;
+        }
+        Node tail = head;
+        Node current = head;
+        // for (Node current = head; current.next != null; current = current.next,tail = current) {
+        // } 也可以
+        while (current != null) {
+            tail = current;
+            current = current.next;
+        }
+
+        tail.next = toAdd;
     }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return simpleQueue.removeFirst();
+        if (head == null) {
+            return -1;
+        }
+        int value = head.value;
+        head = head.next;
+        return value;
+    }
+
+}
+
+class Node {
+    int value;
+    Node next;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public Node(int value, Node next) {
+        this.value = value;
+        this.next = next;
     }
 }
--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -4,14 +4,26 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class Stack {
-    Deque<Integer> simple_Stack = new ArrayDeque<Integer>();
+    private Node head = null;
+
     // 将一个元素压入栈内
     public void push(int value) {
-        simple_Stack.push(value);
+        if (head == null) {
+            head = new Node(value);
+            return;
+        }
+        Node temp = head;
+        head = new Node(value, temp);
     }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return simple_Stack.pop();
+        if (head == null) {
+            return -1;
+        }
+        int value=head.value;
+        head = head.next;
+        return value;
     }
+
 }
