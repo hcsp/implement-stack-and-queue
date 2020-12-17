1cb7da8b92359191558df5405e3439a021fad805
Su <sgz886@gmail.com>
2020-12-17 21:10:02 +0800

    用LinkedList实现队列,用ArrayDeque实现栈

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,17 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Stack {
+    Deque<Integer> simple_Stack = new ArrayDeque<Integer>();
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        simple_Stack.push(value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return simple_Stack.pop();
     }
 }
