0fdf1dc9a577a059bc6d03efd8256a6ed3fd7d96
yang song <1479867360@qq.com>
2020-12-27 20:28:39 +0800

    实现栈和队列 (#96)
    实现栈和队列 (#96)

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,35 @@
 package com.github.hcsp.datastructure;
 
 public class Queue {
+    private Node first;
+    private Node last;
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        if (first == null) {
+            first = last = new Node(value);
+        } else {
+            last.next = new Node(value);
+            last = last.next;
+        }
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        if (first == null) {
+            throw new RuntimeException("没有元素了");
+        }
+        int val = first.val;
+        first = first.next;
+        return val;
+    }
+
+    private static class Node {
+        int val;
+        Node next;
+
+        private Node(int val) {
+            this.val = val;
+        }
     }
 }
