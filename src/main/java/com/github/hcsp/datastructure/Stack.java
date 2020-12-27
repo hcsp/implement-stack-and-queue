95f232ad067557ec0833338a4558624bed793930
iRuby <1479867360@qq.com>
2020-12-27 20:25:43 +0800

    实现栈和队列

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,31 @@
 package com.github.hcsp.datastructure;
 
 public class Stack {
+    private Node last;
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        if (last == null) {
+            last = new Node(value, null);
+        } else {
+            last = new Node(value, last);
+        }
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        int val = last.val;
+        last = last.pre;
+        return val;
+    }
+
+    private static class Node {
+        int val;
+        Node pre;
+
+        public Node(int val, Node pre) {
+            this.val = val;
+            this.pre = pre;
+        }
     }
 }
