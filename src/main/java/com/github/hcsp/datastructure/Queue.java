842718a7404d007d83382c92e7519d7937306442
wizard <42760426+qipengwei@users.noreply.github.com>
2021-01-03 17:53:12 +0800

    实现基本栈 与队列 (#98)
    实现基本栈 与队列 (#98)

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,16 @@
 package com.github.hcsp.datastructure;
 
+import java.util.LinkedList;
+
 public class Queue {
+    java.util.Queue<Integer> queue = new LinkedList<>();
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        queue.offer(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        return queue.poll();
     }
 }
