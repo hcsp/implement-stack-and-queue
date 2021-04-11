d3f10bf7825fe5acee6e47697e3b315f57c2792f
weiranyi <67884344+weiranyi@users.noreply.github.com>
2021-04-11 16:26:03 +0800

    通过动态数组实现队列和栈 (#103)
    通过动态数组实现队列和栈 (#103)

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,22 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stack {
+    /**
+     * 定义：栈是一种特殊的数组，ArrayList又是一种动态数组，那么可以通过ArrayList实现队列
+     * 队列：栈顶才能新增或者删除元素
+     */
+    List<Object> list = new ArrayList<>();
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        list.add(0, value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return (int) list.remove(0);
     }
 }
