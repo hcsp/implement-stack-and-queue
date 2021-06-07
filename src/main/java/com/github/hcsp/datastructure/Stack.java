83db163caad10aa09d53d0ba585d5368dabc59db
giantpetter <65336996+giantpetter@users.noreply.github.com>
2021-06-07 10:13:04 +0800

    实现队列和栈

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,19 @@
 package com.github.hcsp.datastructure;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Stack {
+    Deque<Integer> stack = new LinkedList<>();
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        stack.push(value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+
+        return stack.pop();
     }
 }
