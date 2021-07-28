7ce93f7c97dec53ea8ddfd3d73bf462887115d1a
bowen-wu <33862944+bowen-wu@users.noreply.github.com>
2021-07-28 17:32:54 +0800

    feat: use List implement (#105)
    feat: use List implement (#105)

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,22 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Queue {
+    private final List<Integer> container;
+
+    public Queue() {
+        this.container = new ArrayList<>();
+    }
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        container.add(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        return container.size() > 0 ? container.remove(0) : -1;
     }
 }
