205f34610c6f0e7ae910813b9608026ff517db0b
bowen-wu <33862944+bowen-wu@users.noreply.github.com>
2021-07-28 17:52:57 +0800

    use Array implements (#106)
    use Array implements (#106)

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,28 @@
 package com.github.hcsp.datastructure;
 
 public class Queue {
+    private final Object[] elements;
+    private int head;
+    private int tail;
+
+    public Queue() {
+        this.elements = new Object[16];
+        this.head = 0;
+        this.tail = 0;
+    }
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        elements[tail++] = value;
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        if (head == tail) {
+            return -1;
+        }
+        Object result = elements[head];
+        elements[head++] = null;
+        return (int) result;
     }
 }
