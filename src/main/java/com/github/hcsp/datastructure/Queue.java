5674c4cc3fec6ce897c69fb5e85475f975fe5d0d
bowen-wu <13793248162@163.com>
2021-07-28 16:45:26 +0800

    feat: use List implement

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,31 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Queue {
+    private List<Integer> container = new ArrayList<>();
+
+    private List<Integer> getContainer() {
+        return container;
+    }
+
+    private void setContainer(List<Integer> container) {
+        this.container = container;
+    }
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        List<Integer> container = getContainer();
+        container.add(value);
+        setContainer(container);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        List<Integer> container = getContainer();
+        int value = container.size() > 0 ? container.remove(0) : -1;
+        setContainer(container);
+        return value;
     }
 }
--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,31 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stack {
+    private List<Integer> container = new ArrayList<>();
+
+    public List<Integer> getContainer() {
+        return container;
+    }
+
+    public void setContainer(List<Integer> container) {
+        this.container = container;
+    }
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        List<Integer> container = getContainer();
+        container.add(value);
+        setContainer(container);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        List<Integer> container = getContainer();
+        int value = container.size() > 0 ? container.remove(container.size() - 1) : -1;
+        setContainer(container);
+        return value;
     }
 }
