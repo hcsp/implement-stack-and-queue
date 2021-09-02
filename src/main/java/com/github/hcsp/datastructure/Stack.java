2347a2ac27f8a9628ed0eea5174040183f0990bc
skyszhang <1296003529@qq.com>
2021-09-02 14:34:59 +0800

    implement stack and queue

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,22 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stack {
+    private final List<Integer> container;
+
+    public Stack() {
+        container = new ArrayList<>();
+    }
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        container.add(value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return container.size() > 0 ? container.remove(container.size() - 1) : -1;
     }
 }
