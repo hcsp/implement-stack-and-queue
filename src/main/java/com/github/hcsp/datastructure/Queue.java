f4e3ee19103d8f2c4ce303f6f54139124cb32fa5
jianqi <jianqi.zhao.xian@gmail.com>
2021-10-11 01:13:28 +1100

    job done

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,17 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Queue {
+    private Deque<Integer> queue = new ArrayDeque<>();
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        queue.addLast(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        return queue.removeFirst();
     }
 }
