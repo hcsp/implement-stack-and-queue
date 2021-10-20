02431bbc8bc3e97cde6c89413c265ada7ad3bde2
hahaen <52993809+hahaen@users.noreply.github.com>
2021-10-20 13:07:34 +0800

    栈和队列 (#110)
    栈和队列 (#110)

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,15 @@
 package com.github.hcsp.datastructure;
 
 public class Stack {
+    java.util.Stack<Integer> stack = new java.util.Stack<>();
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        stack.push(value);
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return stack.pop();
     }
 }
