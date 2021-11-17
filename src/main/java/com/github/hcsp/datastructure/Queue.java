cb98809060996322c7033a0e7b51403e846f3e06
Jerry Wang <1976883731@qq.com>
2021-11-17 23:17:54 +0800

    Stack and Queue test

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,19 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Queue {
+    private static final List<Integer> list = new ArrayList<>();
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        list.add(value);
+
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        return list.remove(0);
     }
 }
