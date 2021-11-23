4420b89d997f594614984840465ad74e07934c3d
朱骏杰 <zhujunjie@sundear.com>
2021-11-23 10:43:36 +0800

    finish

--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,19 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+
 public class Stack {
+    ArrayList<Integer> arrayList = new ArrayList<>();
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        arrayList.add(value);
+
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return arrayList.remove(arrayList.size() - 1);
     }
 }
+
