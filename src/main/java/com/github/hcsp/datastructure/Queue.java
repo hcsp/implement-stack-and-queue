2ee27810515861e15bd289632f5e5049e0be837a
soohea <sooheaa@gmai.com>
2022-01-19 14:16:58 +0800

    数据结构练习：实现栈和队列

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,21 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Queue {
+
+    List<Integer> queueList = new ArrayList<>();
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        queueList.add(value);
+
+
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        return queueList.remove(0);
     }
 }
--- src/main/java/com/github/hcsp/datastructure/Stack.java
+++ src/main/java/com/github/hcsp/datastructure/Stack.java
@@ -1,11 +1,19 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stack {
+    List<Integer> stackList = new ArrayList<>();
+
     // 将一个元素压入栈内
-    public void push(int value) {}
+    public void push(int value) {
+        stackList.add(0, value);
+
+    }
 
     // 从栈顶弹出一个元素
     public int pop() {
-        return 0;
+        return stackList.remove(0);
     }
 }
