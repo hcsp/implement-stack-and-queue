5ba591511d92813b2ce2e96b5afafe274e76a6f6
rf4github <39894049+rf818@users.noreply.github.com>
2024-02-28 10:08:31 +0800

    implement-stack-and-queue (#119)
    implement-stack-and-queue (#119)

--- src/main/java/com/github/hcsp/datastructure/Queue.java
+++ src/main/java/com/github/hcsp/datastructure/Queue.java
@@ -1,11 +1,22 @@
 package com.github.hcsp.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Queue {
+    List<Integer> q;
+
+    public Queue() {
+        q = new ArrayList<>();
+    }
+
     // 将一个元素添加到队列尾部
-    public void add(int value) {}
+    public void add(int value) {
+        q.add(value);
+    }
 
     // 将一个元素从队列头部移走
     public int remove() {
-        return 0;
+        return q.remove(0);
     }
 }
